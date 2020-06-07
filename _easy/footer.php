93e7a8d65af331394d4b25f98ec935e281d3633f
sanya buryat <alexandrbnimaev@yandex.ru>
2020-06-07 18:55:59 +0800

    initial commit

---
+++ _easy/footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package starter
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_easy' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', '_easy' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', '_easy' ), '_easy', '<a href="http://weblancer.net/users/_easy/">alexandr balzhinimaev</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
